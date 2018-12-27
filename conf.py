fb4e03267a03514664a393c2133a66995633e5d3
Mikhail Milekhin <mikhail.milekhin@gmail.com>
2018-12-27 11:50:16 +0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -45,15 +45,6 @@ extensions = ['sphinx.ext.autodoc',
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-# recommonmark is a python utility that allows markdown to be used within
-# Sphinx projects.
-# Installed version as per directive in docs/requirement.txt
-from recommonmark.parser import CommonMarkParser
-
-source_parsers = {
-    '.md': CommonMarkParser,
-}
-
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
