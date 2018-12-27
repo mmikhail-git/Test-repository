424f0827656a45ea7bb3d5033437f76d86cdba76
Mikhail Milekhin <mikhail.milekhin@gmail.com>
2018-12-27 13:00:09 +0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -148,25 +148,17 @@ man_pages = [
 
 # -- Options for LaTeX output ---------------------------------------------
 
-latex_engine = 'lualatex'
+latex_engine = 'xelatex'
 
 latex_elements = {
     
 
+
     'fontpkg': r'''
-\setmainfont{DejaVu Serif}
-\setsansfont{DejaVu Sans}
-\setmonofont{DejaVu Sans Mono}
-''',
-    'preamble': r'''
-\usepackage[titles]{tocloft}
-\cftsetpnumwidth {1.25cm}\cftsetrmarg{1.5cm}
-\setlength{\cftchapnumwidth}{0.75cm}
-\setlength{\cftsecindent}{\cftchapnumwidth}
-\setlength{\cftsecnumwidth}{1.25cm}
-''',
-    'fncychap': r'\usepackage[Bjornstrup]{fncychap}',
-    'printindex': r'\footnotesize\raggedright\printindex',
+\usepackage[koi8-r]{inputenc}
+\usepackage[russian]{babel}
+'''
+    
 
     
     # The paper size ('letterpaper' or 'a4paper').
