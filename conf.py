cf5c786e974abe78f8b122de7f5a0acc1c511dba
Mikhail Milekhin <mikhail.milekhin@gmail.com>
2018-12-27 12:52:37 +0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -151,6 +151,24 @@ man_pages = [
 latex_engine = 'lualatex'
 
 latex_elements = {
+    
+
+    'fontpkg': r'''
+\setmainfont{DejaVu Serif}
+\setsansfont{DejaVu Sans}
+\setmonofont{DejaVu Sans Mono}
+''',
+    'preamble': r'''
+\usepackage[titles]{tocloft}
+\cftsetpnumwidth {1.25cm}\cftsetrmarg{1.5cm}
+\setlength{\cftchapnumwidth}{0.75cm}
+\setlength{\cftsecindent}{\cftchapnumwidth}
+\setlength{\cftsecnumwidth}{1.25cm}
+''',
+    'fncychap': r'\usepackage[Bjornstrup]{fncychap}',
+    'printindex': r'\footnotesize\raggedright\printindex',
+
+    
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
